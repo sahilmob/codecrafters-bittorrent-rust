ad5b6402b34095dd8a4bbadf09069b4db456026b
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-26 14:29:19 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     match de::from_str(encoded_value) {
-        Ok(v) => v,
+        Ok(v) => serde_json::Value::String(v),
         Err(e) => panic!("{}", e),
     }
     // // If encoded_value starts with a digit, it's a number
@@ -20,7 +20,7 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     //     let number_string = &encoded_value[..colon_index];
     //     let number = number_string.parse::<i64>().unwrap();
     //     let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-    //     return serde_json::Value::String(string.to_string());
+    //     return ;
     // } else if next_token == &'i' {
     //     let e_index = encoded_value.find('e').unwrap();
     //     let number_string = &encoded_value[1..e_index];
