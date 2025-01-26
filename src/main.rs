74351677e2c8170b8927228fc8ef7d6079bc30b1
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-26 14:42:29 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,46 @@
-use serde_bencode;
 use serde_json::{self};
 use std::env;
 
 // Available if you need it!
 // use serde_bencode
 
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    match serde_bencode::from_str(encoded_value) {
-        Ok(v) => serde_json::Value::String(v),
-        Err(_) => serde_json::Value::String("[]".to_string()),
-    }
-    // // If encoded_value starts with a digit, it's a number
-    // let mut chars = encoded_value.chars().into_iter().peekable();
-    // let next_token = chars.peek().unwrap();
-    // if next_token.is_digit(10) {
-    //     // Example: "5:hello" -> "hello"
-    //     let colon_index = encoded_value.find(':').unwrap();
-    //     let number_string = &encoded_value[..colon_index];
-    //     let number = number_string.parse::<i64>().unwrap();
-    //     let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-    //     return ;
-    // } else if next_token == &'i' {
-    //     let e_index = encoded_value.find('e').unwrap();
-    //     let number_string = &encoded_value[1..e_index];
-    //     let number = number_string.parse::<i64>().unwrap();
-    //     return serde_json::Value::Number(Number::from(number));
-    // } else {
-    //     panic!("Unhandled encoded value: {}", encoded_value)
-    // }
+fn decode_bencoded_value(encoded_value: &str) -> anyhow::Result<serde_json::Value> {
+    let value: serde_bencode::value::Value = serde_bencode::from_str(encoded_value)?;
+    convert(value)
 }
-
+fn convert(value: serde_bencode::value::Value) -> anyhow::Result<serde_json::Value> {
+    match value {
+        serde_bencode::value::Value::Bytes(b) => {
+            let string = String::from_utf8(b)?;
+            Ok(serde_json::Value::String(string))
+        }
+        serde_bencode::value::Value::Int(i) => {
+            Ok(serde_json::Value::Number(serde_json::Number::from(i)))
+        }
+        serde_bencode::value::Value::List(l) => {
+            let array = l
+                .into_iter()
+                .map(|item| convert(item))
+                .collect::<anyhow::Result<Vec<serde_json::Value>>>()?;
+            Ok(serde_json::Value::Array(array))
+        }
+        _ => {
+            panic!("Unhandled encoded value: {:?}", value)
+        }
+    }
+}
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main() {
+
+fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        eprintln!("Logs from your program will appear here!");
-
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let decoded_value = decode_bencoded_value(encoded_value)?;
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
+    Ok(())
 }
